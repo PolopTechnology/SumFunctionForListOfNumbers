b559b0a8569ed7180f78ed63c0391bee03300afb
PolopTech <73751987+PolopTechnology@users.noreply.github.com>
2021-02-08 15:24:26 -0500

    Add files via upload

---
+++ Sum Function for large numbers.py
@@ -0,0 +1,13 @@
+i = int(input())
+times = 0
+nums = []
+finalNum = 0
+while i >= times:
+    b = int(input())
+    nums.append(b)
+    times += 1
+
+for x in nums:
+    finalNum += x
+
+print(finalNum)
